2eac71390285361db8f287c87c583c40dade464d
marko154 <marko.gartnar777@gmail.com>
2024-10-27 01:11:35 +0200

    fix type error

--- metadata-service/src/routes/artists/artists.index.ts
+++ metadata-service/src/routes/artists/artists.index.ts
@@ -6,8 +6,8 @@ import * as routes from "./artists.routes";
 const router = createRouter()
   // .openapi(routes.list, handlers.list)
   // .openapi(routes.create, handlers.create)
-  .openapi(routes.getOne, handlers.get)
-  // .openapi(routes.patch, handlers.patch)
-  // .openapi(routes.remove, handlers.remove);
+  .openapi(routes.getOne, handlers.getOne);
+// .openapi(routes.patch, handlers.patch)
+// .openapi(routes.remove, handlers.remove);
 
 export default router;
