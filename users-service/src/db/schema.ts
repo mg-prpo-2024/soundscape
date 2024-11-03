83c68fcc90d35272fa065a07cb0981efe8de1f1b
marko154 <marko.gartnar777@gmail.com>
2024-11-03 20:53:46 +0100

    add users db container

--- users-service/src/db/schema.ts
+++ users-service/src/db/schema.ts
@@ -1,9 +1,10 @@
-import { integer, pgEnum, pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: uuid().primaryKey().defaultRandom(),
   name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
+  age: integer().notNull(),
   imageUrl: varchar({ length: 255 }),
   stripeCustomerId: varchar({ length: 255 }).notNull().unique(),
   createdAt: timestamp().defaultNow(),
