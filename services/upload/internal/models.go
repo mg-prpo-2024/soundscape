5ef31773c7f525575e68e5e32ab46c92839e47cf
marko154 <marko.gartnar777@gmail.com>
2025-01-06 12:38:13 +0100

    refactor models: introduce base struct and update artist, album, image, and song definitions

--- services/upload/internal/models.go
+++ services/upload/internal/models.go
@@ -1,24 +1,54 @@
 package internal
 
 import (
-	"database/sql"
 	"time"
 
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
-type User struct {
-	ID               uuid.UUID `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
-	Auth0Id          string    `gorm:"unique"`
-	Email            string
-	StripeCustomerId sql.NullString
-	CreatedAt        time.Time // Automatically managed by GORM for creation time
-	UpdatedAt        time.Time // Automatically managed by GORM for update time
+type Base struct {
+	ID        uuid.UUID `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
+	CreatedAt time.Time // Automatically managed by GORM for creation time
+	UpdatedAt time.Time // Automatically managed by GORM for update time
 }
 
+type Artist struct {
+	Base
+	Name   string
+	Bio    string
+	Images []Image
+	Songs  []*Song `gorm:"many2many:artist_songs;"`
+}
+
+type Image struct {
+	Base
+	BlobUrl  string
+	ArtistId uuid.UUID
+}
+
+type Song struct {
+	Base
+	Title      string
+	BlobUrl    string
+	TrackOrder uint
+	AlbumId    uuid.UUID
+	Artists    []*Artist `gorm:"many2many:artist_songs;"`
+}
+
+type Album struct {
+	Base
+	Title string
+	Songs []Song
+}
+
+// type Genre struct {
+// 	base
+// 	Name string
+// }
+
 func AutoMigrate(db *gorm.DB) {
-	err := db.AutoMigrate(&User{})
+	err := db.AutoMigrate(&Artist{}, &Album{}, &Image{}, &Song{})
 	if err != nil {
 		panic(err)
 	}
