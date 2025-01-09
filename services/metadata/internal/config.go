dd6e7a29cc41712dd9e665e7d14f24fa57cac0b0
marko154 <marko.gartnar777@gmail.com>
2025-01-09 14:25:31 +0100

    fix: change service port from 8080 to 8000 in Dockerfile, config, and deployment files

--- services/metadata/internal/config.go
+++ services/metadata/internal/config.go
@@ -9,7 +9,7 @@ import (
 // Options for the CLI.
 type Options struct {
 	AppEnv             string `help:"Application environment (production|local)" default:"local"`
-	Port               int    `help:"Port to listen on" short:"p" default:"8080"`
+	Port               int    `help:"Port to listen on" short:"p" default:"8000"`
 	Auth0Domain        string `help:"Auth0 domain"`
 	Auth0Audience      string `help:"Auth0 audience"`
 	PostgresUrl        string `help:"Host for the Postgres database"`
