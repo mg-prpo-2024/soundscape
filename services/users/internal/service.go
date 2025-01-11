ebc144adfe1a9ee200fa16c974879e8a416a7a19
marko154 <marko.gartnar777@gmail.com>
2025-01-11 21:42:02 +0100

    get customer

--- services/users/internal/service.go
+++ services/users/internal/service.go
@@ -3,6 +3,7 @@ package internal
 import (
 	"github.com/stripe/stripe-go/v81"
 	"github.com/stripe/stripe-go/v81/checkout/session"
+	"github.com/stripe/stripe-go/v81/customer"
 )
 
 type Service interface {
@@ -10,6 +11,7 @@ type Service interface {
 	GetUser(id string) (*UserDto, error)
 	CreateSubscription(priceId, userId, successUrl, cancelUrl string) (*stripe.CheckoutSession, error)
 	ProvisionSubscription(session *stripe.CheckoutSession) error
+	GetCustomer(id string) (*stripe.Customer, error)
 }
 
 type service struct {
@@ -53,4 +55,15 @@ func (s *service) CreateSubscription(priceId, userId, successUrl, cancelUrl stri
 func (s *service) ProvisionSubscription(session *stripe.CheckoutSession) error {
 	userId := session.Metadata["userId"]
 	return s.repo.SetCustomerId(userId, session.Customer.ID)
+}
+
+func (s *service) GetCustomer(userId string) (*stripe.Customer, error) {
+	user, err := s.repo.GetUser(userId)
+	if err != nil {
+		return nil, err
+	}
+	// TODO: extract stripe to a separate repository
+	params := &stripe.CustomerParams{}
+	result, err := customer.Get(*user.StripeCustomerId, params)
+	return result, err
 }
